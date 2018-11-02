5af0032dd283c75b3bda1b33b2339cf097367586
Ranjeet <ranjeet@Akshays-MacBook-Air.local>
2018-11-02 18:24:31 +0530

    Haml demo code added

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,9 @@
 class GreetingsController < ApplicationController
   def hello
+    @datetime = DateTime.now
+  end
+
+  def about
+    @datetime = DateTime.now
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'greetings/hello'
+  root to: 'greetings#hello'
+  get :about, to: 'greetings#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
