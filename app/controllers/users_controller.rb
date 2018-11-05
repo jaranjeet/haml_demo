fc5fd01a534617a717183b2c2de95e45de079f41
jaranjeet <ranjeet.jagtap43@gmail.com>
2018-11-05 13:11:17 +0530

    Figaro, colorize gems added, Sign in action, enviroment variables added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
   end
 
   def sign_in
+    if params[:username] == ENV['EMAIL_USERNAME'] && params[:password] == ENV['EMAIL_PASSWORD']
+      puts 'Correct username or password'.red
+    else
+      puts 'Incorrect username or password'.red
+    end
+    redirect_to root_path
   end
 
   def sign_up
