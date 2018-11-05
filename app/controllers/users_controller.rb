6214b018a5ead733527f1bd3feb065a42f8d3c82
jaranjeet <ranjeet.jagtap43@gmail.com>
2018-11-05 11:10:39 +0530

    Sign in and Sign up forms added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def sign_in
+  end
+
+  def sign_up
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'greetings#hello'
-  get :about, to: 'greetings#about'
+  root to: 'users#index'
+  post :sign_in, to: 'users#sign_in'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
