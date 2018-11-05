8054aed64931b99770f151c31a5eef872ad47bdd
jaranjeet <ranjeet.jagtap43@gmail.com>
2018-11-05 15:02:16 +0530

    welcome, user info views added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,19 @@ class UsersController < ApplicationController
   def sign_in
     if params[:username] == ENV['EMAIL_USERNAME'] && params[:password] == ENV['EMAIL_PASSWORD']
       puts 'Correct username or password'.red
+      session[:username] = params[:username]
+      session[:email] = ENV['EMAIL']
+      redirect_to welcome_path
     else
       puts 'Incorrect username or password'.red
+      flash[:error] = 'Wrong username or password!'
+      render 'index'
     end
-    redirect_to root_path
   end
 
   def sign_up
+  end
+
+  def welcome
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'users#index'
   post :sign_in, to: 'users#sign_in'
+  get :welcome, to: 'users#welcome'
+  get :user_info, to: 'users#user_info'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
